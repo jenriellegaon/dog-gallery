a7460af7cd38501184db6c2077953bcd92690e3a
jenriellegaon <jenriellegaon>
2018-07-26 15:32:06 +0800

    Remove default fab

--- app/src/main/java/gallery/dogs/doggallery/view/MainView.java
+++ app/src/main/java/gallery/dogs/doggallery/view/MainView.java
@@ -1,19 +1,17 @@
 package gallery.dogs.doggallery.view;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import gallery.dogs.doggallery.R;
 import gallery.dogs.doggallery.contract.MainContract;
 import gallery.dogs.doggallery.model.pojo.ResObj;
@@ -22,33 +20,31 @@ import gallery.dogs.doggallery.presenter.listeners.AboutDialog;
 public class MainView extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, MainContract.View {
 
-    @BindView(R.id.fab)
-    FloatingActionButton fab;
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
+    @BindView(R.id.nav_view_left)
+    NavigationView navigationViewLeft;
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_view);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        //Bind Views
+        ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
-
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        ActionBarDrawerToggle toggle;
+        toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationViewLeft = findViewById(R.id.nav_view_left);
         navigationViewLeft.setNavigationItemSelectedListener(this);
         onNavigationItemSelected(navigationViewLeft.getMenu().findItem(R.id.nav_home));
         navigationViewLeft.setCheckedItem(R.id.nav_home);
@@ -65,32 +61,9 @@ public class MainView extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_view, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
