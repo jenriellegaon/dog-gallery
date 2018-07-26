1e313344c28bfe2e9e781420635002bbca3eed01
jenriellegaon <jenriellegaon>
2018-07-26 14:43:26 +0800

    Fix about dialog view

--- app/src/main/java/gallery/dogs/doggallery/presenter/listeners/AboutDialog.java
+++ app/src/main/java/gallery/dogs/doggallery/presenter/listeners/AboutDialog.java
@@ -1,18 +1,23 @@
 package gallery.dogs.doggallery.presenter.listeners;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 
+import com.afollestad.materialdialogs.GravityEnum;
 import com.afollestad.materialdialogs.MaterialDialog;
 
 import gallery.dogs.doggallery.R;
 
 public class AboutDialog {
 
+    @SuppressLint("ResourceAsColor")
     public AboutDialog(final Context context) {
 
         boolean wrapInScrollView = true;
         new MaterialDialog.Builder(context)
-                .title("")
+                .title("About")
+                .titleColor(R.color.colorPrimaryDark)
+                .titleGravity(GravityEnum.CENTER)
                 .customView(R.layout.content_about, wrapInScrollView)
                 .show();
     }
